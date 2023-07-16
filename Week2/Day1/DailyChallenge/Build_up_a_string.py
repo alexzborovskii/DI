230500f0c2c3c8d5ec123ac5733a0412ed3fca72
alexzborovskii <alexzborovskii@gmail.com>
2023-07-16 14:48:30 +0300

    w2d1 DailyChallenge finished

---
+++ Week2/Day1/DailyChallenge/Build_up_a_string.py
@@ -0,0 +1,27 @@
+# 1
+input_string = input("Type in the string: ")
+string_length = len(input_string)
+if string_length < 10:
+    print("string not long enough")
+if string_length> 10:
+    print("string too long")
+else:
+    print("perfect string")
+    
+# 2
+print(input_string[0], input_string[-1])
+
+# 3
+output = ""
+for c in input_string:
+    output += c
+    print(output)
+    
+
+# 4
+import random
+
+input_list = list(input_string)
+random.shuffle(input_list)
+shuffled_output = "".join((input_list))
+print(shuffled_output)
