b1592003bb32b2ddb2392898bb3b09a1c81cf403
alexzborovskii <alexzborovskii@gmail.com>
2023-07-20 12:31:43 +0300

    w2d5 DailyChallenge finished

---
+++ Week2/Day5/DailyChallenge/Daily_challenge.py
@@ -0,0 +1,21 @@
+# # Challenge 1 : Sorting
+# sequance = input("Enter the comma-separeted sequance of words:\n")
+# sequance_list = list(sequance.split(","))
+# sequance_list.sort()
+# result = ",".join(sequance_list)
+# print(result)
+
+# # Challenge 2 : Longest Word
+# def longest_word(string) -> str:
+#     string_list = string.split()
+#     longest_word = ""
+#     longest_word_len = 0
+#     for word in string_list :
+#         if len(word) > longest_word_len :
+#             longest_word = word
+#             longest_word_len = len(word)
+#     return longest_word
+
+# # print(longest_word("Margaret's toy is a pretty doll."))
+# # print(longest_word("A thing of beauty is a joy forever."))
+# # print(longest_word("Forgetfulness is by all means powerless!"))
