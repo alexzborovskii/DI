3347d7c72a7d1aee89843bf039bcc0e89a848e42
alexzborovskii <alexzborovskii@gmail.com>
2023-07-21 12:47:58 +0300

    w2d5 ex-s formated

--- Week2/Day5/ExercisesXP/Mini-Project_#2-Hangman.py
+++ Week2/Day5/ExercisesXP/Mini-Project_#2-Hangman.py
@@ -1,6 +1,7 @@
 import random
 
-wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
+wordslist = ['correction', 'childish', 'beach', 'python', 'assertive',
+             'interference', 'complete', 'share', 'credit card', 'rush', 'south']
 incorrect_turns = 0
 guessed_letters = set()
 pics = ['''
@@ -40,44 +41,46 @@ pics = ['''
    / \  |
        ===''']
 
+
 def game():
     global incorrect_turns
     global asteriscs_left
-    
-    word = random.choice(wordslist) 
+
+    word = random.choice(wordslist)
     length = len(word)
     asteriscs_left = length
     asterisc = "*"
     current_word_list = list(length * asterisc)
     current_word = "".join(current_word_list)
     print(f'The word is : {current_word}')
-    
-    while True :
-    # guess
+
+    while True:
+        # guess
         letter = input("input the letter: ")
         # counting failed guesses
         if letter not in word:
-            print(pics[incorrect_turns])             
+            print(pics[incorrect_turns])
             incorrect_turns += 1
         # showing guessed letters
         else:
-            if letter not in guessed_letters :
+            if letter not in guessed_letters:
                 for i in range(length):
-                    if word[i] == letter :
+                    if word[i] == letter:
                         current_word_list[i] = letter
                         asteriscs_left -= 1
                 guessed_letters.add(letter)
             current_word = "".join(current_word_list)
         print(current_word)
         # fail
-        if incorrect_turns == len(pics) :
+        if incorrect_turns == len(pics):
             print(f"You fail. The word was '{word}'.")
             input()
             break
         #  win
-        if asteriscs_left == 0 :
+        if asteriscs_left == 0:
             print(f"You won! The word is '{word}'.")
             input()
             break
-        
+
+
 game()
