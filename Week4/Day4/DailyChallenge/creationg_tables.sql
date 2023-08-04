8c9faaf74d2d3dcd28a0dba124fac590fe90f285
alexzborovskii <alexzborovskii@gmail.com>
2023-08-04 17:05:17 +0300

    w4d4 daily challenge finished

--- Week4/Day4/DailyChallenge/creationg_tables.sql
+++ Week4/Day4/DailyChallenge/creationg_tables.sql
@@ -1,18 +1,8 @@
 create table country (
 	country_id serial primary key,
-	country_name varchar (50)
-);
-
-create table capital (
-	capital_id serial primary key,
-	capital_name varchar (50),
-	country_id int unique foreign key on country (country_id)
-);
-
-create table additional_info (
-	info_id serial primary key,
-	country_id int unique foreign key on country (country_id),
+	country_name varchar (50),
+	capital_name varchar(100),
 	flag varchar (100),
 	subregion varchar (100),
-	population int
-)
+	population int;
+);
--- Week4/Day4/DailyChallenge/dc-web_api.py
+++ Week4/Day4/DailyChallenge/dc-web_api.py
@@ -1,6 +1,5 @@
 import random
-import requests, json, psycopg2
-from methods import *
+import requests, psycopg2
 
 def country_insert(countries):
     connection = None
@@ -15,52 +14,30 @@ def country_insert(countries):
         
         with connection:
             with connection.cursor() as cursor: 
-                
                 for country in countries:
                     name = country['name']['common']
                     capital = country['capital'][0]
                     flag = country['flag']
                     subregion = country['subregion']
                     population = country['population']
-
-                    name_query = f'''
-                                insert into country (country_name) values ("{name}")
-                                '''
-                    capital_query = f'''
-                                    INSERT INTO capital (capital_name, country_id) values 
-                                    ("{capital}", (select country_id from country where country_name = "{name}"))
-                                    '''
-                    add_info_query = f"""
-                                    INSERT INTO additional_info (country_id, flag, subregion, population) values 
-                                    ((select country_id from country where country_name = '{name}'), '{flag}', '{subregion}', '{population}')
-                                    """
-
-                                      
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(name_query)
-                connection.commit()
-        
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(capital_query)
-                connection.commit()
+                    name = name.replace("'", "''")
+                    capital = capital.replace("'", "''")
+                    flag = flag.replace("'", "''")
+                    subregion = subregion.replace("'", "''")
                 
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(add_info_query)
-                connection.commit()
+                    query = f'''
+                            insert into country 
+                            (country_name, capital_name, flag, subregion, population) 
+                            values ('{name}', '{capital}', '{flag}', '{subregion}', '{population}');
+                            '''
+                    cursor.execute(query)
+                    connection.commit()
                     
-            
-                         
-
     except Exception as e :
         print(e)
     finally :
         if connection != None:
             connection.close()
-            
-    
             
             
 def get_random_countries():
@@ -72,11 +49,9 @@ def get_random_countries():
         else:
             countries_data = response.json()
             random_countries = random.sample(countries_data, 10)
-            print(random_countries)
-            
             return(random_countries)
     except Exception as err:
         print(err)
     
-# print(get_random_countries())
-country_insert(get_random_countries())
+if __name__ == "__main__":
+    country_insert(get_random_countries())
