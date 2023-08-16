ad6ec77731ed983c50f0fa7ca0c353e25d3b5ed9
alexzborovskii <alexzborovskii@gmail.com>
2023-08-16 18:31:44 +0300

    w5d4 daily challenge 2 finished

---
+++ Week6/Day4/DailyChallenge/dc2/script.js
@@ -0,0 +1,17 @@
+// // 1 loop
+// res_str = "*";
+// for (let i = 0; i < 6; i++){
+//     console.log(res_str);
+//     res_str += ' *';
+// }
+
+// // 2 loops
+// res_str2 = "";
+// for (let i = 0; i < 6; i++) {
+//     str_i = "";
+//     for (let j = 0; j <= i; j++){
+//         str_i += '* ';
+//     }
+//     res_str2 += str_i + '\n';
+// }
+// console.log(res_str2);
