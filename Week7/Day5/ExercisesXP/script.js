73a8d93bbacf8b49bb47dfe96e8855a8ba5dc8bb
alexzborovskii <alexzborovskii@gmail.com>
2023-08-24 14:43:44 +0300

    w7d5 exercises XP (robots mini project) finished

---
+++ Week7/Day5/ExercisesXP/script.js
@@ -0,0 +1,128 @@
+const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        image: 'https://robohash.org/1?200x200'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        image: 'https://robohash.org/3?200x200'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        image: 'https://robohash.org/4?200x200'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        image: 'https://robohash.org/5?200x200'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        image: 'https://robohash.org/6?200x200'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        image: 'https://robohash.org/7?200x200'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        image: 'https://robohash.org/8?200x200'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        image: 'https://robohash.org/9?200x200'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        image: 'https://robohash.org/10?200x200'
+    }
+];
+
+const contentBox = document.getElementById("content-box");
+const searchInput = document.getElementById("search_input");
+
+
+const searchResult = () => {
+    console.log(searchInput.value);
+    // console.log("innerHTML; " + contentBox.innerHTML);
+    filterRobots(searchInput.value);
+}
+
+const displayRobotCard = (robot) => {
+    const robopic = document.createElement("img");
+    robopic.src = robot.image;
+    const roboname = document.createElement("p");
+    roboname.innerText = robot.name;
+    const roboemail = document.createElement("p")
+    roboemail.innerText = robot.email;
+    robopic.className = "robopic";
+    roboname.className = "roboname"
+    roboemail.className = "roboemail"
+    const robocard = document.createElement("div");
+    robocard.className = "robocard";
+    robocard.append(robopic, roboname, roboemail);
+    contentBox.append(robocard);
+}
+
+const filterRobots = (inputText) => {
+    contentBox.innerHTML = "";
+    const filteredRobots = robots.filter((robot) => robot.name.toLowerCase().includes(inputText.toLowerCase()));
+    filteredRobots.forEach((element) => displayRobotCard(element));
+    emptyCheck(filteredRobots);
+}
+
+const displayAll = () => {
+    robots.forEach(robot => {
+        displayRobotCard(robot)
+    });
+}
+
+const emptyCheck = (filteredRobots) => {
+    if (filteredRobots.length == 0) {
+        (() => {
+            console.log("empty")
+            const emptyFilter = document.createElement("div");
+            emptyFilter.innerText = "There is no robots with a such name";
+            emptyFilter.className = "empty_div"
+            contentBox.append(emptyFilter);
+        })()
+    }
+}
+
+
+
+displayAll();
+searchInput.addEventListener('input', searchResult);
