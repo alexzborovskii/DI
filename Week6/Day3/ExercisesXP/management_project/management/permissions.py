6cd6b5024cf7f0894ebd0021c543912eb5d49431
alexzborovskii <alexzborovskii@gmail.com>
2023-08-26 13:02:38 +0300

    w6d3 daily challenge redone

--- Week6/Day3/ExercisesXP/management_project/management/permissions.py
+++ Week6/Day3/ExercisesXP/management_project/management/permissions.py
@@ -2,12 +2,11 @@ from rest_framework import permissions
 
 class IsDepartmentAdmin(permissions.BasePermission):
     
-    # Just doesn`t work with object! 
-    # def has_object_permission(self, request, view):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            if request.method in ['GET', 'POST','PUT', 'DELETE']:  
-                return request.user.is_staff 
+        if request.user.is_authenticated and request.method in ['GET', 'POST','PUT', 'DELETE']:  
+            return request.user.is_staff 
         else:
             return False
         
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.administrator
