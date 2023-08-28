fbca0381a102c79d091aa982a312d8999eecc1e6
alexzborovskii <alexzborovskii@gmail.com>
2023-08-28 17:02:50 +0300

    w8d2 dc2 finished

---
+++ Week8/Day2/DailyChallenge/dc2/script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const nameInp = document.getElementById("name");
+const lNameInp = document.getElementById("lastname");
+const butt = document.getElementById("send");
+
+butt.addEventListener('click', display);
+
+function display(event) {
+    event.preventDefault();
+
+    console.log(nameInp.value);
+    console.log(lNameInp.value);
+    const myObj = { "name": nameInp.value, "lastname": lNameInp.value }
+    const myJSONString = JSON.stringify(myObj)
+    const p = document.createElement("p");
+    p.innerText = myJSONString;
+    body.append(p)
+}
